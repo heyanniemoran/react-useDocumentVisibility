f427ae31839a458a3126f8a8f5afbe184fc3ea5f
heyanniemoran <heyanniemoran@gmail.com>
2022-07-17 14:05:03 +0300

    Проверяю, что работает hook, поставила prettier

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import useDocumentVisibility from './useDocumentVisibility'
+import React, { useState, useEffect } from "react";
+import useDocumentVisibility from "@heyanniemoran/use-document-visibility";
 
 const LeaveTabCounter = () => {
+  const { count, visible, onVisibilityChange } = useDocumentVisibility();
 
-    const { count, visible, onVisibilityChange } = useDocumentVisibility();
+  useEffect(() => {
+    onVisibilityChange((isVisible) => {
+      console.log("first handler", isVisible);
+    });
+    onVisibilityChange((isVisible) => {
+      console.log("second handler", isVisible);
+    });
+  }, []);
 
-    useEffect(() => {
-        onVisibilityChange((isVisible) => {
-            console.log('first handler', isVisible)
-        });
-        onVisibilityChange((isVisible) => {
-            console.log('second handler', isVisible)
-        });
-    }, [])
-
-    return (
-        <div>
-          <span>
-            Вы покинули страницу: {count} раз <br />
-            Вкладка активна? {visible ? 'да' : 'нет'}
-          </span>
-        </div>
-    );
+  return (
+    <div>
+      <span>
+        Вы покинули страницу: {count} раз <br />
+        Вкладка активна? {visible ? "да" : "нет"}
+      </span>
+    </div>
+  );
 };
 export default LeaveTabCounter;
